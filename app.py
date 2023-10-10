da40924630f68f37fd10cd5e20a3d2b262b44827
otlevire <eriveltoclenio@gmail.com>
2023-10-10 08:40:55 -0700

    refactor: modularized chat response

--- app.py
+++ app.py
@@ -18,6 +18,29 @@ from dotenv import load_dotenv
 load_dotenv()
 OPENAI_API_KEY = str(os.getenv('OPENAI_API_KEY'))
 
+def get_model_response(user_input):    
+    # Inicialiação o modelo llm
+    llm = OpenAI(temperature=0,                 # criatividade de resposta 
+                model_name='text-davinci-003'  # modelo em uso
+                ) 
+
+    # Instanciando a conversação
+    conversation = ConversationChain(
+        llm=llm,
+        verbose=True,
+        memory=ConversationBufferMemory()
+    )
+
+
+    # Testando a memória na conversa 
+    conversation("Hi, how is it going!")
+    conversation("My name is Erivelto")
+    conversation.predict(input="I stay in hyderabad, Angola")
+    print(conversation.memory.buffer)
+    return conversation.predict(input="What is my name?")
+
+
+
 
 FIRST_CHATBOT_PHASE ="How can I help you Sr.?"
 # Setting title and header on page
@@ -33,26 +56,6 @@ if summarise_button:
     # summarise_placeholder = st.sidebar.write("Nice chatting with you my friend ❤️ :\n\n"+st.session_state['conversation'].memory.buffer)
 
 
-# Inicialiação o modelo llm
-llm = OpenAI(temperature=0,                 # criatividade de resposta 
-             model_name='text-davinci-003'  # modelo em uso
-            ) 
-
-# Instanciando a conversação
-conversation = ConversationChain(
-    llm=llm,
-    verbose=True,
-    memory=ConversationBufferMemory()
-)
-
-
-# Testando a memória na conversa 
-conversation("Hi, how is it going!")
-conversation("My name is Erivelto")
-conversation.predict(input="I stay in hyderabad, Angola")
-print(conversation.memory.buffer)
-conversation.predict(input="What is my name?")
-
 response_container = st.container()
 container = st.container()
 
@@ -60,9 +63,11 @@ with container:
     with st.form(key='my_form', clear_on_submit=True):
         user_input = st.text_area('Your question goes here:', key='input', height=100)
         submit_button = st.form_submit_button(label='Send')
-
-with response_container:
-    st.write("Response container")
-    
-
+        if submit_button: 
+            if user_input == "":
+                st.sidebar.write("Ups😢!!! Enter samething in the form field!")
+            else:
+                answer = get_model_response(user_input=user_input)                
+                with response_container:
+                    st.write(answer)
 
